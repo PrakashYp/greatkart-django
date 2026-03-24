2fecfd7653624b325ba68efa947fa1f3d897c9de
Prakash Yp <ypprakash835@gmail.com>
2026-03-24 18:56:13 +0530

    fix degraded health

--- carts/context_processors.py
+++ carts/context_processors.py
@@ -1,22 +1,25 @@
-from .models import Cart,CartItem
+from .models import Cart, CartItem
 from .views import _cart_id
 
 def counters(request):
     cart_count = 0
+
     if 'admin' in request.path:
         return {}
-    else:
-        try:
-            cart = Cart.objects.filter(cart_id =_cart_id(request))
-            if request.user.is_authenticated:
-                cart_items = CartItem.objects.all().filter(user= request.user)
-            else:
-                cart_items = CartItem.objects.all().filter(cart = cart[:1])
 
-            for cart_item in cart_items:
-                cart_count += cart_item.quantity
-        except Cart.DoesNotExist:
-            cart_count = 0
-    return dict( cart_count = cart_count )
+    try:
+        if request.user.is_authenticated:
+            cart_items = CartItem.objects.filter(user=request.user)
+        else:
+            cart = Cart.objects.filter(cart_id=_cart_id(request)).first()
+            cart_items = CartItem.objects.filter(cart=cart) if cart else []
+
+        for cart_item in cart_items:
+            cart_count += cart_item.quantity
+
+    except Exception:
+        cart_count = 0
+
+    return dict(cart_count=cart_count)
 
 
